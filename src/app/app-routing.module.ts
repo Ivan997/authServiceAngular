2c58e6892c208c01cc3c77685a2cfb722abb9605
Ivan Arredondo <a16310034@ceti.mx>
2020-07-10 21:31:39 -0500

    ya funciona login, solo falta dar permisos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { PrivadaComponent } from './components/privada/privada.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'privada', component: PrivadaComponent},
+  {path: 'precios', component: PreciosComponent},
+  {path: 'callback', component: CallbackComponent},
+  {path: '**', component: HomeComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/privada/privada.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privada',
+  templateUrl: './privada.component.html',
+  styles: [
+  ]
+})
+export class PrivadaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
